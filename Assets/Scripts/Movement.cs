84299403403343758e110229e1c3d5ccd5190542
Stick <rutaspk@gmail.com>
2021-11-26 20:18:08 +0200

    removed camera shots

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public void MoveObject()
     {
-        var newPosition = transform.position;
+        var newPosition = transform.localPosition;
         newPosition = new Vector3(newPosition.x, newPosition.y + Time.deltaTime, newPosition.z);
-        transform.position = newPosition;
+        transform.localPosition += transform.InverseTransformPoint(Vector3.up) * Time.deltaTime;//  newPosition;
+        
     }
 }
+    
--- Assets/Scripts/StarObject.cs
+++ Assets/Scripts/StarObject.cs
@@ -13,6 +13,8 @@ public class StarObject : MonoBehaviour
         rotation = GetComponent<Rotation>();
         
         rotation.RotateRandom();
+        
+        
     }
     
     void Update()
